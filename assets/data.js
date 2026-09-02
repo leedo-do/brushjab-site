4c3be8a99137dbcad7d2128391ce2cafd03cafe8
Brushjab <hello@brushjab.com>
2026-09-03 02:14:22 +0900

    1호 갤러리 15번 작품을 아카이브로 이동
    Canvas 1호 - 015(images/1ho-018.jpg)를 1호 갤러리에서 빼고 아카이브 069번으로 옮김.

--- assets/data.js
+++ assets/data.js
@@ -97,7 +97,6 @@ const SITE_DATA = {
         { image: "images/1ho-021.jpg" },
         { image: "images/1ho-020.jpg" },
         { image: "images/1ho-019.jpg" },
-        { image: "images/1ho-018.jpg" },
         { image: "images/1ho-016.jpg" },
         { image: "images/1ho-015.jpg" },
         { image: "images/1ho-013.jpg" },
@@ -236,6 +235,7 @@ const SITE_DATA = {
       { image: "images/archive-066.jpg" },
       { image: "images/archive-067.jpg" },
       { image: "images/archive-068.jpg" },
+      { image: "images/archive-069.jpg" },
     ],
     // ▲▲▲ 여기까지 ▲▲▲
   },
